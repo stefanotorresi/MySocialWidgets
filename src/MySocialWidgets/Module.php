4750a131b3ed719c0be70a3fb32dcba605e4a16b
Stefano Torresi <webdev@stefanotorresi.it>
2014-01-16 18:14:45 +0100

    Revert "remove zfc-base in favor of my-base"
    This reverts commit c2ce52680817fb987ead9ed7d02056db4995881d.

--- src/MySocialWidgets/Module.php
+++ src/MySocialWidgets/Module.php
@@ -7,9 +7,34 @@
 
 namespace MySocialWidgets;
 
-use MyBase\AbstractModule;
+use Zend\ModuleManager\Feature;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Stdlib\Glob;
+use ZfcBase\Module\AbstractModule;
 
-class Module extends AbstractModule
+class Module extends AbstractModule implements
+    Feature\ConfigProviderInterface
 {
+    public function getConfig()
+    {
+        $config = parent::getConfig();
 
+        $configFiles = Glob::glob($this->getDir().'/config/*.config.php');
+
+        foreach ($configFiles as $configFile) {
+            $config = ArrayUtils::merge($config, include $configFile);
+        }
+
+        return $config;
+    }
+
+    public function getDir()
+    {
+        return __DIR__ . '/../..';
+    }
+
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
 }
