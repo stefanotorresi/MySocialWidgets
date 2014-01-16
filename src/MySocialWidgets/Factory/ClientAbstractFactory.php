59c3f5012e9683855562badc13f324f2b9d19399
Stefano Torresi <webdev@stefanotorresi.it>
2014-01-16 18:03:46 +0100

    cs fix

--- src/MySocialWidgets/Factory/ClientAbstractFactory.php
+++ src/MySocialWidgets/Factory/ClientAbstractFactory.php
@@ -18,9 +18,9 @@ class ClientAbstractFactory implements AbstractFactoryInterface
     /**
      * Determine if we can create a service with name
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @param $name
-     * @param $requestedName
+     * @param  ServiceLocatorInterface $serviceLocator
+     *                                                 @param $name
+     *                                                 @param $requestedName
      * @return bool
      */
     public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
@@ -42,9 +42,9 @@ class ClientAbstractFactory implements AbstractFactoryInterface
     /**
      * Create service with name
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @param $name
-     * @param $requestedName
+     * @param  ServiceLocatorInterface $serviceLocator
+     *                                                 @param $name
+     *                                                 @param $requestedName
      * @return mixed
      */
     public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
--- src/MySocialWidgets/Factory/ViewHelperAbstractFactory.php
+++ src/MySocialWidgets/Factory/ViewHelperAbstractFactory.php
@@ -16,9 +16,9 @@ class ViewHelperAbstractFactory implements AbstractFactoryInterface
     /**
      * Determine if we can create a service with name
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @param $name
-     * @param $requestedName
+     * @param  ServiceLocatorInterface $serviceLocator
+     *                                                 @param $name
+     *                                                 @param $requestedName
      * @return bool
      */
     public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
@@ -34,9 +34,9 @@ class ViewHelperAbstractFactory implements AbstractFactoryInterface
     /**
      * Create service with name
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @param $name
-     * @param $requestedName
+     * @param  ServiceLocatorInterface $serviceLocator
+     *                                                 @param $name
+     *                                                 @param $requestedName
      * @return mixed
      */
     public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
