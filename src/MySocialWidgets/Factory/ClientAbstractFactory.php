e3da363946836123ac1bae5d56824f883e614ba0
Stefano Torresi <webdev@stefanotorresi.it>
2014-01-16 18:18:12 +0100

    cs fix

--- src/MySocialWidgets/Factory/ClientAbstractFactory.php
+++ src/MySocialWidgets/Factory/ClientAbstractFactory.php
@@ -19,8 +19,8 @@ class ClientAbstractFactory implements AbstractFactoryInterface
      * Determine if we can create a service with name
      *
      * @param  ServiceLocatorInterface $serviceLocator
-     *                                                 @param $name
-     *                                                 @param $requestedName
+     * @param $name
+     * @param $requestedName
      * @return bool
      */
     public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
@@ -43,8 +43,8 @@ class ClientAbstractFactory implements AbstractFactoryInterface
      * Create service with name
      *
      * @param  ServiceLocatorInterface $serviceLocator
-     *                                                 @param $name
-     *                                                 @param $requestedName
+     * @param $name
+     * @param $requestedName
      * @return mixed
      */
     public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
--- src/MySocialWidgets/Factory/ViewHelperAbstractFactory.php
+++ src/MySocialWidgets/Factory/ViewHelperAbstractFactory.php
@@ -17,8 +17,8 @@ class ViewHelperAbstractFactory implements AbstractFactoryInterface
      * Determine if we can create a service with name
      *
      * @param  ServiceLocatorInterface $serviceLocator
-     *                                                 @param $name
-     *                                                 @param $requestedName
+     * @param $name
+     * @param $requestedName
      * @return bool
      */
     public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
@@ -35,8 +35,8 @@ class ViewHelperAbstractFactory implements AbstractFactoryInterface
      * Create service with name
      *
      * @param  ServiceLocatorInterface $serviceLocator
-     *                                                 @param $name
-     *                                                 @param $requestedName
+     * @param $name
+     * @param $requestedName
      * @return mixed
      */
     public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
